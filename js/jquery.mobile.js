c47997cf43d8c0bd76361e17d334cdd94195dc99
Shaun Berryman <shaun@shaunberryman.com>
2012-12-03 15:38:30 -0800

    include only tap events code

--- js/jquery.mobile.js
+++ js/jquery.mobile.js
@@ -3,39 +3,11 @@
 
 define([
 	'require',
-	'./widgets/loader',
-	'./jquery.mobile.navigation',
-	'./jquery.mobile.navigation.pushstate',
-	'./jquery.mobile.transitions',
-	'./jquery.mobile.degradeInputs',
-	'./widgets/dialog',
-	'./widgets/page.sections',
-	'./widgets/collapsible',
-	'./widgets/collapsibleSet',
-	'./jquery.mobile.fieldContain',
-	'./jquery.mobile.grid',
-	'./widgets/navbar',
-	'./widgets/listview',
-	'./widgets/listview.filter',
-	'./widgets/listview.autodividers',
-	'./jquery.mobile.nojs',
-	'./widgets/forms/checkboxradio',
-	'./widgets/forms/button',
-	'./widgets/forms/slider',
-	'./widgets/forms/textinput',
-	'./widgets/forms/select.custom',
-	'./widgets/forms/select',
-	'./jquery.mobile.buttonMarkup',
-	'./widgets/controlgroup',
-	'./jquery.mobile.links',
-	'./widgets/fixedToolbar',
-	'./widgets/popup',
-	'./widgets/table',
-	'./widgets/table.columntoggle',
-	'./widgets/table.reflow',
+	'./jquery.mobile.core',
+	'./jquery.mobile.events',
 	'./jquery.mobile.zoom',
 	'./jquery.mobile.zoom.iosorientationfix'
 ], function( require ) {
-	require( [ './jquery.mobile.init' ], function() {} );
+	require( [], function() {} );
 });
 //>>excludeEnd("jqmBuildExclude");
